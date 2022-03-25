1cf0f16360c431fd0f618a3647e6c2b2e2127e4c
Isaiah <ijandicos90@gmail.com>
2022-03-25 00:40:11 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName = "BOB"
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+
+const LeastFavoriteCustomer = "invariable"
+function changeLeastFavoriteCustomer() {
+    return LeastFavoriteCustomer = "variable"
+}
+
